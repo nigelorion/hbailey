5ab1bf2f011b8d2c3bce90cac9395baa78af07c9
Nigel Orion <nigelorion@gmail.com>
2017-09-29 13:59:12 -0700

    initial vue setup

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <nav>
+      <a href="#">home</a>
+      <a href="#">gallery</a>
+      <a href="#">story</a>
+      <a href="#">terms</a>
+      <a href="#">contact</a>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+}
+
+nav {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main">
+    <h1>{{ title }}</h1>
+    <h3>{{ header }}</h3>
+    <p>{{ subHeader }}</p>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+      title: 'h.bailey',
+      header: 'CURATED RESALE',
+      subHeader: "men's wear for all."
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h1, h3, p {
+  font-weight: normal;
+  transition: all 300ms;
+
+  &:hover {
+    color: rgb(215, 215, 215);
+  }
+}
+
+
+</style>
