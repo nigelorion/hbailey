41445a18eed8708e1794cd01794d6b2e8bc5fe3f
Nigel Orion <Nigel Orion>
2017-10-11 16:32:52 -0700

    add gallery fae images to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,10 +6,15 @@
       <h3>{{ header }}</h3>
       <p>{{ subHeader }}</p>
     </div>
+
     <div class="right">
-      <a target="_blank" href="https://www.facebook.com/hilary.b.burnett"><img src="../assets/facebook.png" alt=""></a>
-      <a target="_blank" href="https://www.instagram.com/hilarybaileyburnett/"><img src="../assets/insta.png" alt=""></a>
+      <transition name="fade2" mode="out-in">
+      <!-- <a target="_blank" href="https://www.facebook.com/hilary.b.burnett"><img src="../assets/facebook.png" alt=""></a>
+      <a target="_blank" href="https://www.instagram.com/hilarybaileyburnett/"><img src="../assets/insta.png" alt=""></a> -->
+        <img class="imgGallery" :key='currentImage' :src="images[Math.abs(currentImage) % images.length]" alt="Image Gallery">
+      </transition>
     </div>
+
   </div>
 
 </template>
@@ -22,7 +27,21 @@ export default {
     return {
       title: 'H.Bailey',
       header: 'CURATED RESALE',
-      subHeader: 'menswear for all'
+      subHeader: 'menswear for all',
+      images: ['https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/22228153_10155498356025795_148669247754655773_n.jpg?oh=137badc2c57e472394db643dc377606b&oe=5A7BFD03', 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/22049865_10155482460010795_6342427858041865217_n.jpg?oh=6f391a4c98f1ae1ee8de13eac01c388e&oe=5A7EAEBE', 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/21768055_10155474844855795_4143192661684090217_n.jpg?oh=2bed796e06a98fabe7ee4b3b56874a29&oe=5A3A3FCF', 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/22046034_10155479723630795_6557431837921339062_n.jpg?oh=afb13b34a8d023e71fcee1bd78b0a856&oe=5A88214B'],
+      currentImage: 0,
+      timer: null
+    }
+  },
+  mounted: function () {
+    this.startRotation()
+  },
+  methods: {
+    startRotation: function () {
+      this.timer = setInterval(this.next, 3000)
+    },
+    next: function () {
+      this.currentImage++
     }
   }
 }
@@ -41,27 +60,44 @@ export default {
 }
 
 .left, .right {
-  width: 200px;
-  text-align: center;
+  padding: 3%;
+  height: 250px;
+  width: 250px;
 
-}
-
-.right {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  padding: 10px;
+  margin: 10px;
+
   @media (max-width: 768px) {
-    flex-direction: row;
-    justify-content: flex-start;
-    padding: 0px;
+    height: 230px;
+    width: 230px;
+
+
+  }
+
+
+}
+
+.right {
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+  // flex-direction: column;
+
+  border: solid 1px black;
+  @media (max-width: 768px) {
+    // flex-direction: row;
+    // justify-content: flex-start;
+    // padding: 0px;
   }
 }
 
 h1, h3, p {
   font-weight: normal;
   transition: all 300ms;
+  margin: 0;
 
   &:hover {
     color: rgb(215, 215, 215);
@@ -70,18 +106,36 @@ h1, h3, p {
 
 h1 {
   font-size: 3em;
-  margin-bottom: 0;
+
+  @media (max-width: 768px) {
+
+    // flex-direction: row;
+    // justify-content: flex-start;
+    // padding: 0px;
+  }
 }
 
 h3 {
-  margin-top: 0px;
-  margin-bottom: 2px;
+
   letter-spacing: 2px;
-  font-size: 1.3;
+  font-size: 1.3em;
+  @media (max-width: 768px) {
+    font-size: 1.1em;
+
+
+    // flex-direction: row;
+    // justify-content: flex-start;
+    // padding: 0px;
+  }
 }
 
 p {
   font-size: 1.1em;
+  @media (max-width: 768px) {
+    // flex-direction: row;
+    // justify-content: flex-start;
+    // padding: 0px;
+  }
 
 
 
@@ -92,19 +146,38 @@ p {
 .left {
   border: solid 1px black;
 }
+// img {
+//   height: 20px;
+//   filter: opacity(.4);
+//   transition: all 200ms;
+//   margin: 5px;
+//   @media (max-width: 768px) {
+//     height: 30px;
+//     margin: 10px;
+//   }
+//   &:hover {
+//     filter: opacity(.8);
+//     transform: scale(1.1);
+//   }
+// }
+
+.imgGallery {
+  height: 100%;
+  width: 100%;
+
+
+}
+
 img {
-  height: 20px;
-  filter: opacity(.4);
-  transition: all 200ms;
-  margin: 5px;
-  @media (max-width: 768px) {
-    height: 30px;
-    margin: 10px;
-  }
-  &:hover {
-    filter: opacity(.8);
-    transform: scale(1.1);
-  }
+
+}
+
+.fade2-enter-active, .fade2-leave-active {
+  transition: opacity 0.8s
+}
+
+.fade2-enter, .fade2-leave-active {
+  transition: opacity 0.8s
 }
 
 </style>
